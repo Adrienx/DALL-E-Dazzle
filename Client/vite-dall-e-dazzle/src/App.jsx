34e081c6ae2e89467f70211382879ab9880480b9
Adrienx <71516635+Adrienx@users.noreply.github.com>
2023-07-02 22:08:36 -0400

    OpenAI API configured, CreateImage Feature Added

---
+++ Client/vite-dall-e-dazzle/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useContext } from "react"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import DataContext from "./DataContext"
+import "./App.css"
+
+function App() {
+  const [prompt, setPrompt] = useState("") //state to store user entered prompt
+  const [result, setResult] = useState("") //state to store image link retrieved from API
+  const [loading, setLoading] = useState(true)
+
+  return (
+    <div className="App">
+      <DataContext.Provider
+        value={{ prompt, setPrompt, result, setResult, loading, setLoading }}
+      >
+        <Header />
+        <Main />
+      </DataContext.Provider>
+    </div>
+  )
+}
+
+export default App
---
+++ Client/vite-dall-e-dazzle/src/components/Main.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./Home"
+import ImageGallery from "./ImageGallery"
+import CreateImage from "./CreateImage"
+import UserProfile from "./UserProfile"
+import UserLogin from "./UserLogin"
+
+const Main = () => {
+  return (
+    <div className="routes-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/CreatePrompt" element={<CreateImage />} />
+        <Route path="/ImageGallery" element={<ImageGallery />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
+        <Route path="/UserLogin" element={<UserLogin />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default Main
