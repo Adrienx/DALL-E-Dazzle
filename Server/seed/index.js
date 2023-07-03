da2be2b7c7ba4d262e9fed53a7ec6b1c6ca18d36
Elliot <ellthrowaway2@icloud.com>
2023-07-03 17:15:35 -0400

    started creating seed file with placeholder image/data

--- Server/seed/index.js
+++ Server/seed/index.js
@@ -1,9 +1,19 @@
 const db = require('../db')
 const { GalleryImage } = require('./models')
 
-const GalleryImages = [
-    {
-        imageUrl: "https://res.cloudinary.com/dblhmvfmy/image/upload/v1688413361/img-JRkLt305Rn9MgYl6dEmGwZtW_vuu3qj.png",
-        prompt: "italian greyhound smoking a joint"
-    }
-]
+const main = async () => {
+    const GalleryImages = [
+        {
+            imageUrl: "https://res.cloudinary.com/dblhmvfmy/image/upload/v1688413361/img-JRkLt305Rn9MgYl6dEmGwZtW_vuu3qj.png",
+            prompt: "italian greyhound smoking a joint"
+        }
+    ]
+    await GalleryImage.insertMany(GalleryImages)
+    console.log('gallery images created') // checking images seed to db
+}
+
+const run = async () => {
+    await main()
+    db.close()
+}
+run()
