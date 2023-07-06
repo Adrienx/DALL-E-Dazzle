674e8b9f81392924ab7a219b7cb5405ab7388f54
Adrienx <71516635+Adrienx@users.noreply.github.com>
2023-07-06 11:16:56 -0400

    sync up image routes with main code

--- Server/controllers/galleryImageController.js
+++ Server/controllers/galleryImageController.js
@@ -1,53 +1,63 @@
 const { GalleryImage } = require("../models/index.js")
 
+const index = async (req, res) => {
+  try {
+    const galleryImage = await GalleryImage.find()
+    res.json(galleryImage)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
 // WORKING:
 const getGalleryImages = async (req, res) => {
-    const images = await GalleryImage.find({})
-    res.json(images)
+  const images = await GalleryImage.find({})
+  res.json(images)
 }
 
 // WORKING:
 const getGalleryImageById = async (req, res) => {
-    try {
-      const { id } = req.params
-      console.log(id) // check id being pulled
-        const image = await GalleryImage.findById(id)
-        if (!image) throw Error("Image not found")
-        res.json(image)
-    } catch (error) {
-        console.log(error)
-        res.send("Error: Image not found")
-    }
+  try {
+    const { id } = req.params
+    console.log(id) // check id being pulled
+    const image = await GalleryImage.findById(id)
+    if (!image) throw Error("Image not found")
+    res.json(image)
+  } catch (error) {
+    console.log(error)
+    res.send("Error: Image not found")
+  }
 }
 
 // WORKING:
 const createGalleryImage = async (req, res) => {
-    try {
-        const newImage = await new GalleryImage(req.body)
-        await newImage.save()
-        return res.status(201).json({ newImage })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
+  try {
+    const newImage = await new GalleryImage(req.body)
+    await newImage.save()
+    return res.status(201).json({ newImage })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
 
 // WORKING:
 const deleteGalleryImage = async (req, res) => {
-    try {
-        const { id } = req.params
-        const deletedImage = await GalleryImage.findByIdAndDelete(id)
-        if (deletedImage) {
-            return res.status(200).send("Image deleted")
-        }
-        throw new Error("Error: Image not found")
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+  try {
+    const { id } = req.params
+    const deletedImage = await GalleryImage.findByIdAndDelete(id)
+    if (deletedImage) {
+      return res.status(200).send("Image deleted")
     }
+    throw new Error("Error: Image not found")
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
 
 module.exports = {
-    getGalleryImages,
-    getGalleryImageById,
-    createGalleryImage,
-    deleteGalleryImage,
+  index,
+  getGalleryImages,
+  getGalleryImageById,
+  createGalleryImage,
+  deleteGalleryImage,
 }
--- Server/routes/AppRouter.js
+++ Server/routes/AppRouter.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const promptRoutes = require("./promptRoutes")
 const promptCategoriesRoutes = require("./promptCategoriesRoutes")
+const galleryImagesRouter = require("./galleryImagesRouter")
 
 router.use("/prompts", promptRoutes)
 router.use("/promptCategories", promptCategoriesRoutes)
+router.use("/gallery", galleryImagesRouter)
 
 module.exports = router
